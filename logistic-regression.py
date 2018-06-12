56fae8b2c389aa956279b771e2b5b36ce7b577c1
Mayank <mayankchaurasia.bsp@gmail.com>
2018-06-13 02:52:58 +0530

    linear and logistic regression done

---
+++ logistic-regression.py
@@ -0,0 +1,174 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import time
+from sklearn.datasets import load_iris
+from sklearn.cross_validation import train_test_split
+import matplotlib.pyplot as plt
+
+iris = load_iris()
+iris_X, iris_y = iris.data[:-1,:], iris.target[:-1]
+iris_y= pd.get_dummies(iris_y).values  # one hot encoding
+trainX, testX, trainY, testY = train_test_split(iris_X, iris_y, test_size=0.33, random_state=42)
+
+
+
+# numFeatures is the number of features in our input data.
+# In the iris dataset, this number is '4'.
+numFeatures = trainX.shape[1]
+
+# numLabels is the number of classes our data points can be in.
+# In the iris dataset, this number is '3'.
+numLabels = trainY.shape[1]
+
+
+# Placeholders
+# 'None' means TensorFlow shouldn't expect a fixed number in that dimension
+X = tf.placeholder(tf.float32, [None, numFeatures]) # Iris has 4 features, so X is a tensor to hold our data.
+yGold = tf.placeholder(tf.float32, [None, numLabels]) # This will be our correct answers matrix for 3 classes.
+
+
+
+'''
+
+y = WX + b
+
+For any training example think of X as [ 0.1  0.2  0.3  0.4 ]  --- (1X4 matrix)
+
+We want our output y as [P(label1)  P(label2)  P(label3) ] ----- (1X3 matrix) 
+
+
+Corresponding W (weight matrix will be) [[w11 w12 w13],
+                                         [w21 w22 w23],
+                                         [w31 w32 w33],
+                                         [w41 w42 w43]]
+    (4X3 matrix) column i is for label i
+
+    
+'''
+
+
+W = tf.Variable(tf.zeros([4,3]))
+b = tf.Variable(tf.zeros([3]))
+
+
+#Randomly sample from a normal distribution with standard deviation .01
+
+weights = tf.Variable(tf.random_normal([numFeatures,numLabels],
+                                       mean=0,
+                                       stddev=0.01,
+                                       name="weights"))
+
+bias = tf.Variable(tf.random_normal([1,numLabels],
+                                    mean=0,
+                                    stddev=0.01,
+                                    name="bias"))
+
+
+apply_weights_OP = tf.matmul(X,weights,name="apply_weights")
+add_bias_OP = tf.add(apply_weights_OP,bias,name="apply_bias")
+activation_OP = tf.nn.sigmoid(add_bias_OP,name="activation")
+
+
+# Defining our learning rate iterations (decay)
+learningRate = tf.train.exponential_decay(learning_rate=0.0008,
+                                          global_step= 1,
+                                          decay_steps=trainX.shape[0],
+                                          decay_rate= 0.95,
+                                          staircase=True)
+
+
+
+#Defining our cost function - Squared Mean Error
+cost_OP = tf.nn.l2_loss(activation_OP-yGold, name="squared_error_cost")
+
+#Defining our Gradient Descent
+training_OP = tf.train.GradientDescentOptimizer(learningRate).minimize(cost_OP)
+
+
+# Create a tensorflow session
+sess = tf.Session()
+
+# Initialize our weights and biases variables.
+init_OP = tf.global_variables_initializer()
+
+# Initialize all tensorflow variables
+sess.run(init_OP)
+
+
+
+
+
+# argmax(activation_OP, 1) returns the label with the most probability
+# argmax(yGold, 1) is the correct label
+correct_predictions_OP = tf.equal(tf.argmax(activation_OP,1),tf.argmax(yGold,1))
+
+# If every false prediction is 0 and every true prediction is 1, the average returns us the accuracy
+accuracy_OP = tf.reduce_mean(tf.cast(correct_predictions_OP, "float"))
+
+# Summary op for regression output
+activation_summary_OP = tf.summary.histogram("output", activation_OP)
+
+# Summary op for accuracy
+accuracy_summary_OP = tf.summary.scalar("accuracy", accuracy_OP)
+
+# Summary op for cost
+cost_summary_OP = tf.summary.scalar("cost", cost_OP)
+
+# Summary ops to check how variables (W, b) are updating after each iteration
+weightSummary = tf.summary.histogram("weights", weights.eval(session=sess))
+biasSummary = tf.summary.histogram("biases", bias.eval(session=sess))
+
+# Merge all summaries
+merged = tf.summary.merge([activation_summary_OP, accuracy_summary_OP, cost_summary_OP, weightSummary, biasSummary])
+
+# Summary writer
+writer = tf.summary.FileWriter("summary_logs", sess.graph)
+
+
+
+
+
+
+# Initialize reporting variables
+cost = 0
+diff = 1
+epoch_values = []
+accuracy_values = []
+cost_values = []
+numEpochs = 700
+
+# Training epochs
+for i in range(numEpochs):
+    if i > 1 and diff < .0001:
+        print("change in cost %g; convergence."%diff)
+        break
+    else:
+        # Run training step
+        step = sess.run(training_OP, feed_dict={X: trainX, yGold: trainY})
+        result = sess.run(weights)
+        # see the weights after each epoch
+        print(result)
+        print("\n")
+        # Report occasional stats
+        if i % 10 == 0:
+            # Add epoch to epoch_values
+            epoch_values.append(i)
+            # Generate accuracy stats on test data
+            train_accuracy, newCost = sess.run([accuracy_OP, cost_OP], feed_dict={X: trainX, yGold: trainY})
+            # Add accuracy to live graphing variable
+            accuracy_values.append(train_accuracy)
+            # Add cost to live graphing variable
+            cost_values.append(newCost)
+            # Re-assign values for variables
+            diff = abs(newCost - cost)
+            cost = newCost
+
+            #generate print statements
+            #print("step %d, training accuracy %g, cost %g, change in cost %g"%(i, train_accuracy, newCost, diff))
+
+
+# How well do we perform on held-out test data?
+print("final accuracy on test set: %s" %str(sess.run(accuracy_OP, 
+                                                     feed_dict={X: testX, 
+                                                                yGold: testY})))
